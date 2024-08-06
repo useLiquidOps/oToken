3d706e29ad637116e82f2de0d8d8f5e1792ebe46
Marton Lederer <marton@lederer.hu>
2024-08-06 13:34:06 +0200

    feat: rework ao utility

--- src/process.lua
+++ src/process.lua
@@ -1,15 +1,23 @@
+Handlers = require ".utils.handlers"
+Utils = require ".utils.utils"
+ao = require ".utils.ao"
+
 local process = { _version = "0.0.1" }
 
-local ao = require ".utils.ao"
-
 function process.handle(msg, env)
-  assert(ao.isTrusted(msg), "ao Message is not trusted")
+  -- setup env
+  local setup_res = ao.init(msg, env)
 
-  ao.normalize_tags(msg)
+  if not setup_res then
+    ao.send({
+      Target = msg.From,
+      Data = "Message or assignment not trusted"
+    })
 
-  return env.result({
-    Output = ""
-  })
+    return ao.result()
+  end
+
+  return ao.result()
 end
 
 return process
