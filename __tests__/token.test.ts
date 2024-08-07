fa76d4ddf4a3ce957fecb45b27c243b406211d60
Marton Lederer <marton@lederer.hu>
2024-08-07 17:13:47 +0200

    fix: env

--- __tests__/token.test.ts
+++ __tests__/token.test.ts
@@ -1,38 +1,57 @@
 import { expect, test, describe, beforeAll } from "bun:test";
-import AoLoader from "@permaweb/ao-loader";
+import AoLoader, { type Environment } from "@permaweb/ao-loader";
 import { env } from "./utils";
 import fs from "fs/promises";
 import path from "path";
 
 describe("Token standard functionalities", () => {
   let handle: AoLoader.handleFunction;
+  let memory: ArrayBuffer | null = null;
 
   beforeAll(async () => {
     const wasmBinary = await fs.readFile(path.join(__dirname, "../src/process.wasm"));
     // @ts-expect-error
-    handle = await AoLoader(wasmBinary, {
-      format: "wasm32-unknown-emscripten2",
-      inputEncoding: "JSON-1",
-      outputEncoding: "JSON-1", 
-      memoryLimit: "524288000",
-      computeLimit: 9e12.toString(),
-      extensions: []
-    })
+    handle = await AoLoader(wasmBinary, { format: "wasm32-unknown-emscripten2" });
   });
 
   test("Returns token info", async () => {
-    const res = handle(null, {
-      Target: env.process.id,
-      Owner: env.process.owner,
-      From: env.process.owner,
+    const res = handle(memory, {
+      Target: env.Process.Id,
+      Owner: env.Process.Owner,
+      From: env.Process.Owner,
       Tags: [
         { name: "Action", value: "Info" }
       ],
       Cron: false,
       "Block-Height": "1",
       Timestamp: "172302981"
-    }, env);
+    }, (env as unknown) as Environment);
 
-    console.log(res);
+    expect(res.Messages).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          Tags: expect.arrayContaining([
+            expect.objectContaining({
+              name: "Name",
+              value: expect.any(String)
+            }),
+            expect.objectContaining({
+              name: "Ticker",
+              value: expect.any(String)
+            }),
+            expect.objectContaining({
+              name: "Logo",
+              value: expect.any(String)
+            }),
+            expect.objectContaining({
+              name: "Denomination",
+              value: expect.any(String)
+            })
+          ])
+        })
+      ])
+    )
+
+    memory = res.Memory;
   });
 });
--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,13 +1,24 @@
-import type { Environment } from "@permaweb/ao-loader";
+import type { Environment, Message } from "@permaweb/ao-loader";
+import { expect } from "bun:test";
 
-export const env: Environment = {
-  process: {
-    id: "0000000000000000000000000000000000000000000",
-    owner: "0000000000000000000000000000000000000000001",
-    tags: [
+export const env = {
+  Process: {
+    Id: "0000000000000000000000000000000000000000000",
+    Owner: "0000000000000000000000000000000000000000001",
+    Tags: [
       { name: "Name", value: "loToken" },
       { name: "Ticker", value: "TST" },
       { name: "Logo", value: "0000000000000000000000000000000000000000002" }
     ]
   }
 };
+
+export function expectMessage(expected: Partial<Message>) {
+  return expect.objectContaining({
+    Owner: expected.Owner || expect.any(String),
+    Target: expected.Target || expect.any(String),
+    Tags: expected.Tags ? expect.arrayContaining(expected.Tags) : undefined,
+    Data: expected.Data || undefined,
+    From: expected.From || expect.any(String)
+  });
+}
