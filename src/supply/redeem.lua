886f88ada4d12f011956fcd259f120bb838d0dee
Marton Lederer <marton@lederer.hu>
2024-08-22 14:40:27 +0200

    feat: redeem wip

--- src/supply/redeem.lua
+++ src/supply/redeem.lua
@@ -1,3 +1,29 @@
-local mod = {}
+local assertions = require ".utils.assertions"
+local bint = require ".utils.bint"(1024)
 
-return mod
+---@type HandlerFunction
+local function redeem(msg)
+  assert(
+    assertions.isTokenQuantity(msg.Tags.Quantity),
+    "Invalid incoming transfer quantity"
+  )
+
+  -- amount of tokens to burn
+  local quantity = bint(msg.Tags.Quantity)
+
+  -- amount of tokens to be sent out
+  local rewardQty = quantity
+
+  -- total tokens pooled
+  local totalPooled = Available + Lent
+
+  -- if the total pooled and the total supply is not
+  -- the same, then the reward qty will be higher
+  -- than the burn qty, because there was already
+  -- some interest coming in
+  if not bint.eq(totalPooled, TotalSupply) then
+    
+  end
+end
+
+return redeem
