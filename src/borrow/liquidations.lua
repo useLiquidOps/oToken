52da970c67486f61a43689e2a9dc57a21c84c91e
Marton Lederer <marton@lederer.hu>
2024-08-24 10:41:54 +0200

    feat: more comments

--- src/borrow/liquidations.lua
+++ src/borrow/liquidations.lua
@@ -1,3 +1,5 @@
 local mod = {}
 
+
+
 return mod
--- src/token/transfer.lua
+++ src/token/transfer.lua
@@ -6,6 +6,7 @@ local function transfer(msg)
   -- transfer target
   local target = msg.Tags.Recipient or msg.Target
 
+  -- validate target and quantity
   assert(assertions.isAddress(target), "Invalid address")
   assert(target ~= msg.From, "Target cannot be the sender")
   assert(
@@ -15,12 +16,15 @@ local function transfer(msg)
 
   local quantity = bint(msg.Tags.Quantity)
 
+  -- validate user balance
   assert(Balances[msg.From] ~= nil, "No balance for this user")
   assert(bint.ule(quantity, Balances[msg.From]), "Not enought tokens for this transfer")
 
+  -- update balances
   Balances[target] = (Balances[target] or bint.zero()) + quantity
   Balances[msg.From] = Balances[msg.From] - quantity
 
+  -- send notices about the transfer
   if not msg.Tags.Cast then
     local debitNotice = {
       Target = msg.From,
@@ -35,6 +39,7 @@ local function transfer(msg)
       Quantity = tostring(quantity)
     }
 
+    -- forwarded tags
     for tagName, tagValue in pairs(msg.Tags) do
       if string.sub(tagName, 1, 2) == "X-" then
         debitNotice[tagName] = tagValue
