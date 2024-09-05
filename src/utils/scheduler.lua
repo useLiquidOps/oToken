eb894039238eb1a838270a10823be397780a93b7
Marton Lederer <marton@lederer.hu>
2024-09-05 20:31:15 +0200

    feat: scheduler wip

---
+++ src/utils/scheduler.lua
@@ -0,0 +1,42 @@
+local coroutine = require "coroutine"
+
+local mod = {}
+
+-- Schedule a batch of messages and wait for all of them
+-- to return. Uses lua coroutines under the hood
+---@param ... MessageParam
+---@return Message[]
+function mod.schedule(...)
+  -- get the running handler's thread
+  local thread = coroutine.running()
+
+  -- repsonse handler
+  local responses = {}
+  local messages = {...}
+
+  ---@type HandlerFunction
+  local function responseHandler(msg)
+    table.insert(responses, msg)
+
+    -- continue execution when all responses are back
+    if #responses == #messages then
+      coroutine.resume(thread, messages)
+    end
+  end
+
+  -- send messages
+  for _, msg in ipairs(messages) do
+    ao.send(msg)
+
+    -- wait for response
+    Handlers.once(
+      { From = msg.Target, ["X-Reference"] = tostring(ao.reference) },
+      responseHandler
+    )
+  end
+
+  -- yield execution, till all responses are back
+  return coroutine.yield()
+end
+
+return mod
