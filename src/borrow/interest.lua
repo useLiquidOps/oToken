dbad957beb928a7dd1028ad01dd8f0c55b300d6c
Marton Lederer <marton@lederer.hu>
2024-09-24 17:24:13 +0200

    feat: sync interests

--- src/borrow/interest.lua
+++ src/borrow/interest.lua
@@ -1,8 +1,6 @@
-local mod = {}
+local bint = require ".utils.bint"(1024)
 
-function mod.calculateInterest()
-  
-end
+local mod = {}
 
 ---@type HandlerFunction
 function mod.syncInterests(msg)
@@ -11,7 +9,31 @@ function mod.syncInterests(msg)
 
   if interestDelay == 0 then return end
 
+  -- helper values for calculation
+  local zero = bint.zero()
+  local totalLent = bint(Lent)
+  local totalPooled = totalLent + bint(Available)
+  local interestDelayB = bint(interestDelay)
+  local oneYearInMs = bint("31560000000")
 
+  -- go through all Loans and add the interest
+  for address, rawQty in pairs(Loans) do
+    if rawQty ~= nil and rawQty ~= "0" then
+      -- loan quantity and interest quantity
+      local loanQty = bint(rawQty)
+      local interestQty = Interests[address] and bint(Interests[address]) or zero
+      local yieldingQty = loanQty + interestQty
+
+      -- calculate missing interest
+      local ownedInterest = bint.udiv(
+        yieldingQty * totalLent * interestDelayB,
+        totalPooled * oneYearInMs
+      )
+
+      -- add owned interest
+      Interests[address] = tostring(interestQty + ownedInterest)
+    end
+  end
 end
 
 return mod
