d1c00baae6d553a33cec099a848c3fcd789f4c0c
Marton Lederer <marton@lederer.hu>
2024-11-07 11:34:38 +0100

    feat: liquidate position wip

--- src/liquidations/liquidate.lua
+++ src/liquidations/liquidate.lua
@@ -6,6 +6,7 @@ local utils = require ".utils.utils"
 local mod = {}
 
 -- Accepts the tokens paid for the liquidation by the liquidator
+-- (similar functionality to repaying)
 ---@type HandlerFunction
 function mod.liquidateBorrow(msg)
   assert(
@@ -110,9 +111,61 @@ function mod.refund(msg, _, err)
 end
 
 -- Transfers out the position to the liquidator
+-- (reverse redeem)
 ---@type HandlerFunction
 function mod.liquidatePosition(msg)
-  
+  assert(
+    assertions.isTokenQuantity(msg.Tags.Quantity),
+    "Invalid quantity"
+  )
+
+  -- amount of tokens to transfer out
+  local quantity = bint(msg.Tags.Quantity)
+
+  -- liquidation target
+  local target = msg.Tags["Liquidation-Target"]
+
+  -- check if the user owns enough tokens for this transfer
+  -- by checking the if the value of the supplied quantity
+  -- in terms of the oToken is less than or equal to the
+  -- amount of oTokens owned
+  local balance = bint(Balances[target] or 0)
+
+  assert(bint.eq(balance, bint.zero()), "Not enough tokens owned by the user to liquidate")
+
+  -- helpers
+  local totalSupply = bint(TotalSupply)
+  local availableTokens = bint(Available)
+  local totalPooled = availableTokens + bint(Lent)
+
+  -- validate if there are enough available tokens
+  assert(
+    bint.ule(quantity, availableTokens),
+    "Not enough tokens available to liquidate"
+  )
+
+  -- get supplied quantity value
+  -- total supply is 100
+  -- total pooled is 5
+  -- 5 incoming = 100 oToken
+  -- (total supply / total pooled) * incoming
+  local qtyValueInoToken = bint.udiv(
+    totalSupply * totalPooled,
+    quantity
+  )
+
+  -- validate with oToken balance
+  assert(
+    bint.ule(qtyValueInoToken, balance),
+    "The user owns less oTokens than the supplied quantity's worth"
+  )
+
+  -- liquidate position by updating the reserves, etc.
+  Balances[target] = tostring(balance - qtyValueInoToken)
+  Available = tostring(availableTokens - quantity)
+  TotalSupply = tostring(totalSupply - qtyValueInoToken)
+
+  -- TODO: notify, transfer
 end
 
 return mod
