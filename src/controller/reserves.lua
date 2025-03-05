19c43c4fbd8abbad4c882a71123d2aa07ca8884a
Marton Lederer <marton@lederer.hu>
2025-03-05 18:35:08 +0100

    feat: deploy

--- src/controller/reserves.lua
+++ src/controller/reserves.lua
@@ -32,15 +32,35 @@ function mod.withdraw(msg)
   })
 
   -- reply
-  msg.reply({
-    ["Withdrawn-Quantity"] = msg.Tags.Quantity,
-    ["Total-Reserves"] = Reserves
-  })
+  msg.reply({ ["Total-Reserves"] = Reserves })
 end
 
 ---@type HandlerFunction
 function mod.deploy(msg)
-  
+  assert(
+    assertions.isTokenQuantity(msg.Tags.Quantity),
+    "Invalid borrow quantity"
+  )
+
+  -- amount of tokens to deploy
+  local quantity = bint(msg.Tags.Quantity)
+  local reserves = bint(Reserves)
+
+  -- check if there are enough tokens to deploy
+  assert(
+    bint.ule(quantity, reserves),
+    "Not enough tokens available to deploy"
+  )
+
+  -- update reserves and cash
+  Reserves = tostring(reserves - quantity)
+  Cash = tostring(bint(Cash) + quantity)
+
+  -- reply
+  msg.reply({
+    ["Total-Reserves"] = Reserves,
+    Cash = Cash
+  })
 end
 
 return mod
