a71306508688d8c73201a207412f9ad285d9ea70
Marton Lederer <marton@lederer.hu>
2025-04-08 13:14:14 +0200

    feat: remediate #15

--- src/controller.lua
+++ src/controller.lua
@@ -895,9 +895,9 @@ Handlers.add(
 ---@param addr any Address to verify
 ---@return boolean
 function assertions.isAddress(addr)
-  if not type(addr) == "string" then return false end
+  if type(addr) ~= "string" then return false end
   if string.len(addr) ~= 43 then return false end
-  if string.match(addr, "[A-z0-9_-]+") == nil then return false end
+  if string.match(addr, "^[A-z0-9_-]+$") == nil then return false end
 
   return true
 end
