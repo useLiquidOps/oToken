8cca52d932d1c85677b230b80d83e7c7163d6fce
Marton Lederer <marton@lederer.hu>
2025-04-30 21:58:24 +0200

    fix: precision for proportional mintQty

--- src/supply/mint.lua
+++ src/supply/mint.lua
@@ -40,7 +40,7 @@ function mint.handler(msg)
   if not bint.eq(totalPooled, bint.zero()) then
     -- mint in proportion to the already supplied tokens
     mintQty = bint.udiv(
-      totalSupply * rawQuantity,
+      totalSupply * quantity,
       totalPooled
     )
   end
