feee1d4c30391a6d46c2d2c4f964cdc7e7efab3b
Marton Lederer <marton@lederer.hu>
2025-07-14 18:49:41 +0200

    feat: prevent recursive errors

--- src/utils/handlers.lua
+++ src/utils/handlers.lua
@@ -570,7 +570,10 @@ function handlers.evaluate(msg, env)
   end
 
   -- make sure the request was handled
-  assert(handled or msg.Id == ao.id, "The request could not be handled")
+  assert(
+    handled or msg.Id == ao.id or string.match(msg.Action or "", "Error$"),
+    "The request could not be handled"
+  )
 end
 
 return handlers
